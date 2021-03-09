863d9095254c4019951d30f89e5345d9b169983c
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-03-09 17:25:04 -0600

    Project Creation

---
+++ cmake-build-debug/CMakeFiles/C___learning.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/robprellwitz/Desktop/Development Projects/CLionProjects/Learn/C++ learning/main.cpp" "/Users/robprellwitz/Desktop/Development Projects/CLionProjects/Learn/C++ learning/cmake-build-debug/CMakeFiles/C___learning.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/C___learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___learning.dir/main.cpp.o"
+  "C___learning"
+  "C___learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
